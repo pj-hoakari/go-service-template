d757808da78df53674e28739d37db74ae69f535e
KasumiMercury <88318012+KasumiMercury@users.noreply.github.com>
2026-07-08 16:32:55 +0900

    feat: Wire up connect service to server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,10 +1,19 @@
 package server
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/pj-hoakari/go-service-template/gen/greet/v1/greetv1connect"
+	"github.com/pj-hoakari/go-service-template/internal/greet"
+)
 
 func NewHandler() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /healthz", handleHealthz)
+
+	path, handler := greetv1connect.NewGreetServiceHandler(greet.NewService())
+	mux.Handle(path, handler)
+
 	return mux
 }
 
